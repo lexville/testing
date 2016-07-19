532961f6e9aac49ba5e0acbd05eb68728bada32b
lexville <alex.kangethe@andela.com>
2016-07-19 13:04:43 +0300

    Add testCanViewUsersPosts testcase inside ViewUsersPostsTest

--- tests/ViewUsersPostsTest.php
+++ tests/ViewUsersPostsTest.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\User;
+use App\Post;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ViewUsersPostsTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -14,5 +18,27 @@ class ViewUsersPostsTest extends TestCase
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+    public function testCanViewUsersPosts()
+    {
+        $user = factory(User::class)->create([
+            'name' => 'hordor',
+        ]);
+
+        factory(Post::class, 5)->create();
+
+        $post = factory(User::class)->make([
+            'post' => 'Hurray my very first post'
+        ]);
+
+        $user->posts()->save($post);
+
+        $this->visit('/posts')
+            ->see('Hurray my very first post');
+
+        $allPosts = Post::all();
+
+        $this->count(6, $allPosts);
     }
 }
