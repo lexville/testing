f4ef54ea701ecea0293b72e5bb4af0f36b3fcb23
lexville <alex.kangethe@andela.com>
2016-07-19 14:58:21 +0300

    Make show method get all posts and show them in posts.show view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function show()
+    {
+        $posts = Post::all();
+
+        return view('posts.show', ['posts' => $posts]);
+    }
 }
